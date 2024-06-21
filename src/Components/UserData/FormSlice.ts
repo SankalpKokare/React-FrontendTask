a1298dd9e5cd6babe9efd98324c7e7b544b4faba
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-06-21 15:03:54 +0530

    fix height and empty json parse in store

--- src/Components/UserData/FormSlice.ts
+++ src/Components/UserData/FormSlice.ts
@@ -1,26 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const getInitialState = () => {
-  const storedFormData = localStorage.getItem('formData');
-  return storedFormData ? JSON.parse(storedFormData) : {
-    firstName: '',
-    lastName: '',
-    phoneNumber: '',
-    email: ''
-  };
+  const storedFormData = localStorage.getItem("formData");
+  return storedFormData
+    ? JSON.parse(storedFormData)
+    : {
+        userDate: { firstName: "", lastName: "", phoneNumber: "", email: "" },
+      };
 };
 
 export const formSlice = createSlice({
-  name: 'form',
+  name: "form",
   initialState: {
-    data: getInitialState()
+    data: getInitialState(),
   },
   reducers: {
     setFormData: (state, action) => {
       state.data = action.payload;
-      localStorage.setItem('formData', JSON.stringify(state.data));
-    }
-  }
+      localStorage.setItem("formData", JSON.stringify(state.data));
+    },
+  },
 });
 
 export const { setFormData } = formSlice.actions;
