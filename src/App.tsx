537eeedac42c39e5aa53fa9ee88baaec2d047eaa
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-06-22 02:12:15 +0530

    added auth0, privateroutes
    Added auth0 for signin and signout
    navigation bar updates when user is logged in
    added private route for dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
 import "./App.css";
 import Counter from "./Components/Counter/Counter";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import UserData from "./Components/UserData/UserData";
 import RichTextEditor from "./Components/TextEditor/RichTextEditor";
 import Navbar from "./Components/NavigationBar/Navbar";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+import SignIn from "./Components/Auth0/SignIn/Signin";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import PrivateRoutes from "./Components/Auth0/SignIn/PriveteRoutes";
 
 function App() {
   return (
-    <BrowserRouter>
-        <Navbar/>
-      <Routes>
-        <Route path="/" element={<Counter />} />
-        <Route path="/userdata" element={<UserData />} />
-        <Route path="/texteditor" element={<RichTextEditor />} />
-      </Routes>
-    </BrowserRouter>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Counter />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/userdata" element={<UserData />} />
+          <Route path="/texteditor" element={<RichTextEditor />} />
+          <Route element={<PrivateRoutes />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Auth0Provider>
   );
 }
 
